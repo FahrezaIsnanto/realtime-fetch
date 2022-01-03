36fc5417899e8f6f3982c26c2a9f7bea06d54cb9
FahrezaIsnanto <mfahrezaisnanto@students.undip.ac.id>
2022-01-03 17:26:40 +0700

    splitting script from index

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(fetchData,1000);
+
+function fetchData(){
+    const url = "https://kelompok2-gmedia.herokuapp.com/tampilbaru";
+    fetch(url)
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        let kel2 = data[0];
+        const date = new Date();
+        document.getElementById("date-update").innerHTML = `Date Update : ${date.toLocaleTimeString()}`;
+        document.getElementById("time-update").innerHTML = `Time Update : ${kel2.time}`;
+        document.getElementById("currentac").innerHTML = `Current AC : ${kel2.currentac}`;
+        document.getElementById("currentdc").innerHTML = `Current DC : ${kel2.currentdc}`;
+        document.getElementById("voltageac").innerHTML = `Voltage AC : ${kel2.voltageac}`;
+        document.getElementById("voltagedc").innerHTML = `Voltage DC : ${kel2.voltagedc}`;
+    })
+    .catch(function(err){
+                console.log("error",err);
+    });
+}
