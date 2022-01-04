39a715f499f777ed82359a50254535e2b340ac40
FahrezaIsnanto <mfahrezaisnanto@students.undip.ac.id>
2022-01-04 17:52:55 +0700

    fix for fetching all data

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-setInterval(fetchData,1000);
+var interval = setInterval(fetchData,1000);
 
 function fetchData(){
-    const url = "https://kelompok2-gmedia.herokuapp.com/tampilbaru";
+    const url = "https://kelompok2-gmedia.herokuapp.com/tampil1";
     fetch(url)
     .then(function(res){
         return res.json();
     })
     .then(function(data){
-        let kel2 = data[0];
-        const date = new Date();
-        document.getElementById("date-update").innerHTML = `Date Update : ${date.toLocaleTimeString()}`;
-        document.getElementById("time-update").innerHTML = `Time Update : ${kel2.time}`;
-        document.getElementById("currentac").innerHTML = `Current AC : ${kel2.currentac}`;
-        document.getElementById("currentdc").innerHTML = `Current DC : ${kel2.currentdc}`;
-        document.getElementById("voltageac").innerHTML = `Voltage AC : ${kel2.voltageac}`;
-        document.getElementById("voltagedc").innerHTML = `Voltage DC : ${kel2.voltagedc}`;
+        const tbody = document.getElementById('data-ins');
+        tbody.innerHTML = '';
+        data.map((map)=>{
+            insertData(map,tbody);
+        });
     })
     .catch(function(err){
-                console.log("error",err);
+        console.log("error",err);
     });
+}
+
+function insertData(map,tbody)
+{
+    var {id,currentac,currentdc,voltageac,voltagedc,time} = map;
+
+    var newRow = document.createElement('tr');
+    var newData1 = document.createElement('td');
+    var newData2 = document.createElement('td');
+    var newData3 = document.createElement('td');
+    var newData4 = document.createElement('td');
+    var newData5 = document.createElement('td');
+    var newData6 = document.createElement('td');
+
+    newData1.innerHTML = id;
+    newData2.innerHTML = currentac;
+    newData3.innerHTML = currentdc;
+    newData4.innerHTML = voltageac;
+    newData5.innerHTML = voltagedc;
+    newData6.innerHTML = time;
+
+    newRow.appendChild(newData1);
+    newRow.appendChild(newData2);
+    newRow.appendChild(newData3);
+    newRow.appendChild(newData4);
+    newRow.appendChild(newData5);
+    newRow.appendChild(newData6);
+    
+    tbody.appendChild(newRow);
 }
